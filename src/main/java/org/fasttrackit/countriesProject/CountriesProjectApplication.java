b25201e16da60a774735df24a7759cede2f51ecd
Groza Marius <mariusgroza520@gmail.com>
2023-06-06 12:48:37 +0300

    firstCommit

---
+++ src/main/java/org/fasttrackit/countriesProject/CountriesProjectApplication.java
@@ -0,0 +1,13 @@
+package org.fasttrackit.countriesProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountriesProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountriesProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/fasttrackit/countriesProject/CountriesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fasttrackit.countriesProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountriesProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
